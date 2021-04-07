580f266acfb75e82a06925400138c4274dfb99f9
David Poulin-Gagnon <dpoulingagnon@nexapp.ca>
2021-04-07 14:31:14 -0400

    add file

--- index.ts
+++ index.ts
@@ -3,7 +3,12 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket("pulumi-trial-bucket");
+
+const bucketObject = new aws.s3.BucketObject("index.html", {
+    bucket: bucket,
+    source: new pulumi.asset.FileAsset("index.html")
+});
 
 // Export the name of the bucket
-export const patate = bucket.id;
+export const bucketName = bucket.id;
