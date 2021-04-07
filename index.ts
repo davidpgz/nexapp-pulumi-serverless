4f3adadaf60c5e36eeb4c7c625f37346652f0c70
David Poulin-Gagnon <dpoulingagnon@nexapp.ca>
2021-04-07 15:03:34 -0400

    test

--- index.ts
+++ index.ts
@@ -1,22 +1,3 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import { apiEndpoint } from "./stacks/api"
+import {bucketEndpoint, bucketName } from "stacks/bucket"
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("pulumi-trial-bucket", {
-    website: {
-        indexDocument: "index.html",
-    }
-});
-
-const bucketObject = new aws.s3.BucketObject("index.html", {
-    acl: "public-read",
-    contentType: "text/html",
-    bucket: bucket,
-    source: new pulumi.asset.FileAsset("index.html")
-});
-
-export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
-
-// Export the name of the bucket
-export const bucketName = bucket.id;
---
+++ stacks/api.ts
@@ -0,0 +1,27 @@
+import * as awsx from "@pulumi/awsx";
+
+const endpoint = new awsx.apigateway.API("hello", {
+    routes: [
+        // Serve static files from the `www` folder (using AWS S3)
+        {
+            path: "/",
+            localPath: "www",
+        },
+
+        // Serve a simple REST API on `GET /name` (using AWS Lambda)
+        {
+            path: "/source",
+            method: "GET",
+            eventHandler: (req, ctx, cb) => {
+                cb(undefined, {
+                    statusCode: 200,
+                    body: Buffer.from(JSON.stringify({ name: "AWS" }), "utf8").toString("base64"),
+                    isBase64Encoded: true,
+                    headers: { "content-type": "application/json" },
+                })
+            }
+        }
+    ]
+});
+
+export const apiEndpoint = endpoint.url;
